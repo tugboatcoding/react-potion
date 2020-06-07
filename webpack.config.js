5ea187603012c469a5ff7181d36d5297e893e031
Mario Hayashi <mario.hayashi@gmail.com>
2020-06-07 02:05:23 +0200

    build: Add webpack config and build script

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const package = require('./package.json');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread'],
+          },
+        },
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'index.js',
+    library: package.name,
+    libraryTarget: 'umd',
+  },
+  externals: {
+    "react": {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    },
+    "react-dom": {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom'
+    }
+  },
+};
