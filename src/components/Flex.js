c16bf539230902d2cfc9c0919bfa805d2c1c0d78
Mario Hayashi <mario.hayashi@gmail.com>
2020-06-10 20:34:15 +0200

    fix: Fix Flex to use custom theme

--- src/components/Flex.js
+++ src/components/Flex.js
@@ -1,3 +1,20 @@
-import { Flex } from 'rebass/styled-components';
+import React from 'react';
+import { Flex as BaseFlex } from 'rebass/styled-components';
+
+const Flex = (props) => (
+  <BaseFlex
+    {...props}
+    theme={{
+      breakpoints: [
+        '414px',
+        '768px',
+        '1024px',
+        '1280px',
+        '1920px',
+      ],
+      space: [],
+    }}
+  />
+);
 
 export default Flex;
---
+++ src/components/Flex.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Box from './Box';
+import Flex from './Flex';
+import Text from './Text';
+
+storiesOf('Flex', module)
+  .add('Default', () => {
+    return (
+      <>
+        <Flex padding={4}><Text>Padded Flex</Text></Flex>
+        <Flex flexDirection={['column','column', 'row']}>
+          <Box marginRight={8}>
+            <Text>Column in mobile</Text>
+          </Box>
+          <Text>Row in desktop</Text>
+        </Flex>
+        <Flex padding={['14px', '24px', '32px', '48px', '72px']}><Text>Responsive padded Flex</Text></Flex>
+      </>
+    );
+  });
