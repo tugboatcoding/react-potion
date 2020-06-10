d46940ad8f0566c2e2fa64c48b47c5190b502bd5
Mario Hayashi <mario.hayashi@gmail.com>
2020-06-10 12:14:07 +0200

    docs: Add story example for padding

--- src/components/Box.stories.js
+++ src/components/Box.stories.js
@@ -7,6 +7,7 @@ storiesOf('Box', module)
   .add('Default', () => {
     return (
       <>
+        <Box padding={4}><Text>Padded box</Text></Box>
         <Box padding={32}><Text>Padded box</Text></Box>
         <Box padding={['14px', '24px', '32px', '48px', '72px']}><Text>Responsive padded box</Text></Box>
       </>
