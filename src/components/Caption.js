c06d04afd78c9ee19a85ea35dfa51dc2ce448d94
Mario Hayashi <mario.hayashi@gmail.com>
2020-09-28 01:40:20 +0200

    feat: Add Caption component

---
+++ src/components/Caption.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Outer = styled.div`
+  font-size: 14px;
+  line-height: 1.4;
+  color: rgba(55, 53, 47, 0.6);
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol";
+  padding-top: 6px;
+  padding-bottom: 6px;
+`;
+
+const Strong = ({ children }) => (
+  <Outer>
+    {children}
+  </Outer>
+);
+
+export default Strong;
