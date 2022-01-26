80c98b7c844a956bd6b5acce42b569c066b5299e
Mario Hayashi <>
2022-01-26 00:56:16 +0000

    fix: Fix Toggle component

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -35,7 +35,7 @@ const Toggle = ({ className, label, children }) => {
         <div>
           <div>
             {!open && label && (
-              <Text {...label} />
+              <Text {...label.props} />
             )}
           </div>
           <div>
